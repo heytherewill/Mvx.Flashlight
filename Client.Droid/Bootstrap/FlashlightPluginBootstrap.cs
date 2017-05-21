8852791d5689690b3028cd6deb2098f9e6fa50ff
William Barbosa <william.sb@hotmail.com>
2017-05-20 23:46:17 -0300

    :electric_plug: Add Android sample project

---
+++ Client.Droid/Bootstrap/FlashlightPluginBootstrap.cs
@@ -0,0 +1,7 @@
+﻿using MvvmCross.Platform.Plugins;
+
+namespace Client.Droid.Bootstrap
+{
+    public class FlashlightScanPluginBootstrap
+        : MvxLoaderPluginBootstrapAction<Flashlight.PluginLoader, Flashlight.Droid.Plugin> { }
+}
